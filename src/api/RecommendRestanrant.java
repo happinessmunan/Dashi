98c50ee738b93b6c180709559333e4d0ff642c8c
Nan Zhang <nanzhang@Nans-MacBook-Pro.local>
2017-05-10 23:43:11 -0700

    First commit

---
+++ src/api/RecommendRestanrant.java
@@ -0,0 +1,58 @@
+package api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class RecommendRestanrant
+ */
+@WebServlet("/recommendation")
+public class RecommendRestanrant extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static DBConnection connection = new MySQLDBConnection();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RecommendRestanrant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		JSONArray array = null;
+
+		if (request.getParameterMap().containsKey("user_id")) {
+			String userId = request.getParameter("user_id");
+			array = connection.recommendRestaurants(userId);
+		}
+		RpcParser.writeOutput(response, array);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/api/SearchRestaurants.java
@@ -0,0 +1,65 @@
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class SearchRestaurants
+ */
+@WebServlet("/restaurants")
+public class SearchRestaurants extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchRestaurants() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		JSONArray array = new JSONArray();
+		DBConnection connection = new MySQLDBConnection();
+		if (request.getParameterMap().containsKey("lat") && request.getParameterMap().containsKey("lon")) {
+			// term is null or empty by default
+			String term = request.getParameter("term");
+			// String userId = (String) session.getAttribute("user");
+			String userId = "1111";
+			double lat = Double.parseDouble(request.getParameter("lat"));
+			double lon = Double.parseDouble(request.getParameter("lon"));
+			array = connection.searchRestaurants(userId, lat, lon, term);
+		}
+		RpcParser.writeOutput(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
